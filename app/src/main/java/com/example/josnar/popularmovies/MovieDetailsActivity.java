cd8f0d56b4154e8237f1f0600223cb29222ca406
Jose Ignacio Naranjo <joseignacio.naranjo@gmail.com>
2017-02-23 19:49:05 +0100

    Trailers are open after item click

--- app/src/main/java/com/example/josnar/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/josnar/popularmovies/MovieDetailsActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -55,6 +56,7 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
         ExpandableHeightListView trailerListView = (ExpandableHeightListView) findViewById(R.id.trailer_list_view);
         trailerListView.setAdapter(mMovieTrailersAdapter);
         trailerListView.setExpanded(true);
+        trailerListView.setOnItemClickListener(mMovieTrailersAdapter);
 
         getSupportLoaderManager().initLoader(TRAILER_LIST_LOADER, null, this);
     }
--- app/src/main/java/com/example/josnar/popularmovies/MovieTrailersAdapter.java
+++ app/src/main/java/com/example/josnar/popularmovies/MovieTrailersAdapter.java
@@ -1,11 +1,12 @@
 package com.example.josnar.popularmovies;
-
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -15,15 +16,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
 
-
-public class MovieTrailersAdapter extends ArrayAdapter<TrailerItem> {
+public class MovieTrailersAdapter extends ArrayAdapter<TrailerItem> implements AdapterView.OnItemClickListener {
 
     public MovieTrailersAdapter(Context context, int resource) {
         super(context, resource);
-
     }
 
     public void populateTrailerList(JSONObject jsonData) {
@@ -55,5 +52,13 @@ public class MovieTrailersAdapter extends ArrayAdapter<TrailerItem> {
         TextView trailerTitleTextView = (TextView) convertView.findViewById(R.id.trailer_title_text_view);
         trailerTitleTextView.setText(trailerItem.title);
         return convertView;
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String videoId = getItem(position).key;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://youtube.com/watch?v=" + videoId));
+        getContext().startActivity(intent);
     }
 }
